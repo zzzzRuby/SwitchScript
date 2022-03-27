fdb9c4121821144b3eeff8743bc5a0f191fab048
zzzzRuby <zzzzRuby@hotmail.com>
2022-03-27 18:27:46 +0800

    clean deps

--- vm/src/emulator/vm_millis.c
+++ vm/src/emulator/vm_millis.c
@@ -4,12 +4,12 @@
 static struct timespec g_VMStartTime;
 
 void VM_MilliSeconds_Init(void) {
-	timespec_get(&g_VMStartTime, TIME_UTC);
+	(void)timespec_get(&g_VMStartTime, TIME_UTC);
 }
 
 uint32_t VM_MilliSeconds(void) {
 	struct timespec now;
-	timespec_get(&now, TIME_UTC);
+	(void)timespec_get(&now, TIME_UTC);
 
 	uint64_t delta_ms = (now.tv_sec - g_VMStartTime.tv_sec) * 1000 + (now.tv_nsec - g_VMStartTime.tv_nsec) / 1000000;
 
